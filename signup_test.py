5c7efd91d24c777d2fca3a227bb3e4b4da571c05
toelapiut <toelapiut7@gmail.com>
2017-10-13 11:48:19 +0300

    add comments

--- signup_test.py
+++ signup_test.py
@@ -1,18 +1,22 @@
-import unittest
-from locker import Credential
+import unittest#importing unittest
+from locker import Credential #importing Credentials
 
+#creating a unittest 
 class Test(unittest.TestCase):
 
+#creating a set up test 
 	def setUp(self):
 
 		self.new_credential=Credential("apiut","toel","toelapiut7@gmail.com","Hello, This are my notes and hope they work")
 
+#creation of an initializing test 
 	def test_init(self):
 		self.assertEqual(self.new_credential.first_name,"apiut")
 		self.assertEqual(self.new_credential.last_name,"toel")
 		self.assertEqual(self.new_credential.email,"toelapiut7@gmail.com")
 		self.assertEqual(self.new_credential.note,"Hello, This are my notes and hope they work")
 
+#testing if my credentials are being saved
 	def test_save_credential(self):
 		self.new_credential.save_credential()
 
