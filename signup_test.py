52d9d0ba52b1dfa2be8345d5db4d5ebd866bbe40
toelapiut <toelapiut7@gmail.com>
2017-10-13 11:42:34 +0300

    fix bug

--- signup_test.py
+++ signup_test.py
@@ -5,14 +5,20 @@ class Test(unittest.TestCase):
 
 	def setUp(self):
 
-		self.new_credential("apiut","toel","toelapiut7@gmail.com","Hello, This are my notes and hope they work")
+		self.new_credential=Credential("apiut","toel","toelapiut7@gmail.com","Hello, This are my notes and hope they work")
 
+	def test_init(self):
 		self.assertEqual(self.new_credential.first_name,"apiut")
 		self.assertEqual(self.new_credential.last_name,"toel")
 		self.assertEqual(self.new_credential.email,"toelapiut7@gmail.com")
-		self.assertEqual(self.new_credential.notes,"This are my notes and hope they work")
+		self.assertEqual(self.new_credential.note,"Hello, This are my notes and hope they work")
+
+	# def test_save_credential(self):
+	# 	self.new_credential.save_credential()
+
+	# 	self.assertEqual(len(Credential.login_credentials),1)
 
 
 
 if __name__=="__main__":
-	unittest.main(verbosity=0)
+	unittest.main(verbosity=2) 
