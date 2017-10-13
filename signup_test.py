77a577cd05366823b849198c910879fc159b374f
toelapiut <toelapiut7@gmail.com>
2017-10-13 12:30:47 +0300

    update

--- signup_test.py
+++ signup_test.py
@@ -36,6 +36,8 @@ class Test(unittest.TestCase):
 
 		self.assertEqual(len(Credential.login_credentials),2)
 
+#delete credential if someone doesn't need the account 
+	def 
 
 if __name__=="__main__":
-	unittest.main() 
+	unittest.main(verbosity=2) 
