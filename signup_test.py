886c1f438845cf49a09321b4fc403ad5729c0746
toelapiut <toelapiut7@gmail.com>
2017-10-13 11:28:59 +0300

    added first test

--- signup_test.py
+++ signup_test.py
@@ -1,10 +1,18 @@
-def checkPalindrome(inputString):
-	inputString.split(' ')
-	# print(inputString)
-	print(inputString.pop())
-	# print(inputed)
-    # if input==inputString:
-    #     return True
-    # else:
-    #     return False
-checkPalindrome('bob')
+import unittest
+from locker import Credential
+
+class Test(unittest.TestCase):
+
+	def setUp(self):
+
+		self.new_credential("apiut","toel","toelapiut7@gmail.com","Hello, This are my notes and hope they work")
+
+		self.assertEqual(self.new_credential.first_name,"apiut")
+		self.assertEqual(self.new_credential.last_name,"toel")
+		self.assertEqual(self.new_credential.email,"toelapiut7@gmail.com")
+		self.assertEqual(self.new_credential.notes,"This are my notes and hope they work")
+
+
+
+if __name__=="__main__":
+	unittest.main(verbosity=0)
