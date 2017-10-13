2c332691b66e9122234affbca867e63bf8ebcf3c
toelapiut <toelapiut7@gmail.com>
2017-10-13 11:15:12 +0300

    add test unit

--- locker.py
+++ locker.py
@@ -1,9 +1,9 @@
-class Login:
+class Credential:
 
 	login_credentials=[]
 
 	def __init__(self,first_name,last_name,email,note):
-		
+
 		self.first_name=first_name
 		self.last_name=last_name
 		self.email=email
