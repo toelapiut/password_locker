489b5d6e9c7da5f1d1e737de037af3ecf50458e3
toelapiut <toelapiut7@gmail.com>
2017-10-13 15:46:27 +0300

    add save multiple and save sing test pass

--- account_test.py
+++ account_test.py
@@ -13,16 +13,23 @@ class Tests(unittest.TestCase):
 		self.assertEqual(self.new_account.emails,"jamesgitau67@gmail.com")
 		self.assertEqual(self.new_account.passwords,"qwertyui")
 
+#save accounts 
+	def test_save_accounts(self):
+		self.new_account.save_accounts()
+		self.assertEqual(len(Account.accounts_list),1)
 
+#tear down clears everything
+	def tearDown(self):
+		Account.accounts_list=[]
 
-
+#save multiple contacts
 	def test_save_multiple_accounts(self):
 		self.new_account.save_accounts()
 
 		test_account=Account("james6@gmail.com","qwe123")
 		test_account.save_accounts()
 		self.assertEqual(len(Account.accounts_list),2)
-		
+
 #display account list
 	def test_dispaly_account(self):
 		self.assertEqual(Account.display_accounts(),Account.accounts_list)
