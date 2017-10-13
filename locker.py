c760935b976cd5129c405a866e1b1185dba2d7c7
toelapiut <toelapiut7@gmail.com>
2017-10-13 11:08:38 +0300

    add class and objects

---
+++ locker.py
@@ -0,0 +1,10 @@
+class Login:
+
+	login_credentials=[]
+
+	def __init__(self,first_name,last_name,email,note):
+		
+		self.first_name=first_name
+		self.last_name=last_name
+		self.email=email
+		self.note=note
