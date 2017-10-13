838b531bff91a0154bed968d167476b4923cfb3a
toelapiut <toelapiut7@gmail.com>
2017-10-13 15:21:05 +0300

    account_test added

---
+++ account_test.py
@@ -0,0 +1,74 @@
+import unittest
+from account import Account
+
+
+class Tests(unittest.TestCase):
+
+	def setUp(self):
+
+		self.new_account=Account("jamesgitau67@gmail.com","qwertyui")
+
+#creation of an initializing test 
+	def test_init(self):
+		self.assertEqual(self.new_account.emails,"jamesgitau67@gmail.com")
+		self.assertEqual(self.new_account.passwords,"qwertyui")
+
+
+#display account list
+	def test_dispaly_accounts(self):
+		self.assertEqual(Account.display.accounts(),Account.accounts)
+		
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #testing if my credentials are being saved
+# 	def test_save_credential(self):
+# 		self.new_credential.save_credential()
+
+# 		self.assertEqual(len(Credential.login_credentials),1)
+
+# #tearing Down function to clear class variable
+# 	def tearDown(self):
+# 		Credential.login_credentials=[]
+
+# #saving multiple credentials 
+# 	def test_save_multiple_credentials(self):
+
+# 		self.new_credential.save_credential()
+
+# 		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+# 		another_credential.save_credential()
+
+# 		self.assertEqual(len(Credential.login_credentials),2)
