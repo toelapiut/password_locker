123d7df6ad757ef861f50db2934d45f0607df711
toelapiut <toelapiut7@gmail.com>
2017-10-13 11:07:27 +0300

    modified

---
+++ sign_up.py
@@ -0,0 +1,10 @@
+def checkPalindrome(inputString):
+	inputString.split(' ')
+	# print(inputString)
+	print(inputString.pop())
+	# print(inputed)
+    # if input==inputString:
+    #     return True
+    # else:
+    #     return False
+checkPalindrome('bob')
