623eb05d6a8ea2455643cf89275090b45f6be3cd
toelapiut <toelapiut7@gmail.com>
2017-10-14 09:24:11 +0300

    del file

--- locker.py
+++ locker.py
@@ -1,4 +1,3 @@
-from account import Account
 import random
 
 main_list=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","1","2","3","4","5","6","7","8","9","0","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","!","@","#","$","%","^","&","*","(",")","_","-","+","?","/"]
@@ -7,30 +6,15 @@ class Credential:
 
 	login_credentials=[]
 
-	def __init__(self,first_name,last_name,password,email):
+	def __init__(self,first_name,last_name,password):
 
 		self.first_name=first_name
 		self.last_name=last_name
 		self.password=password
-		self.email=email
-
  
 
-   
-	def generate_password():
-		g_password=[]	
-		randspasslength=random.randint(10,15)
-		count=0
-		while count<=randspasslength:
-			count+=1
-			indexes=[random.randint(0,62)]	
-			for x in indexes:
-				passx=main_list[x]
-			g_password.append(passx)
-		g_password.append(passx)
-		x=''.join(g_password)
-		print(x)
-	generate_password()
+
+
 			
 
 #Methods to describe the functionality 
@@ -60,8 +44,45 @@ class Credential:
 
 
 	@classmethod
-	def account_exist(cls,email):
+	def account_exist(cls,last_name):
 		for femail in cls.login_credentials:
-			if femail.email==email:
+			if femail.last_name==last_name:
 				return True
 		return False
+
+
+class Account:
+
+	accounts_list=[]
+
+	def __init__(self,user_name,passwords):
+
+		self.emails=emails
+		self.passwords=passwords
+
+
+
+##password generatoe
+	def generate_password():
+		g_password=[]	
+		randspasslength=random.randint(10,15)
+		count=0
+		while count<=randspasslength:
+			count+=1
+			indexes=[random.randint(0,62)]	
+			for x in indexes:
+				passx=main_list[x]
+			g_password.append(passx)
+		g_password.append(passx)
+		x=''.join(g_password)
+		account_info=x
+	generate_password()
+
+#end of password generator	
+
+	def save_accounts(self):
+		Account.accounts_list.append(self)
+
+	@classmethod
+	def display_accounts(cls):
+		return cls.accounts_list
--- signup_test.py
+++ signup_test.py
@@ -7,14 +7,14 @@ class Test(unittest.TestCase):
 #creating a set up test 
 	def setUp(self):
 
-		self.new_credential=Credential("apiut","toel","QwertY","toelapiut7@gmail.com")
+		self.new_credential=Credential("apiut","toel","QwertY")
 
 #creation of an initializing test 
 	def test_init(self):
 		self.assertEqual(self.new_credential.first_name,"apiut")
 		self.assertEqual(self.new_credential.last_name,"toel")
 		self.assertEqual(self.new_credential.password,"QwertY")
-		self.assertEqual(self.new_credential.email,"toelapiut7@gmail.com")
+		
 
 #testing if my credentials are being saved
 	def test_save_credential(self):
@@ -31,7 +31,7 @@ class Test(unittest.TestCase):
 
 		self.new_credential.save_credential()
 
-		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+		another_credential=Credential("James","Gitau","QWERTY")
 		another_credential.save_credential()
 
 		self.assertEqual(len(Credential.login_credentials),2)
@@ -41,7 +41,7 @@ class Test(unittest.TestCase):
 
 		self.new_credential.save_credential()
 
-		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+		another_credential=Credential("James","Gitau","QWERTY")
 		another_credential.save_credential()
 
 		self.new_credential.delete_credential()
@@ -52,26 +52,89 @@ class Test(unittest.TestCase):
 	def find_account_by_password(self):
 		self.new_credential.save_credential()
 
-		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+		another_credential=Credential("James","Gitau","QWERTY")
 		another_credential.save_credential()
 
 		account_found=Credential.find_account("QWERTY")
 
-		self.assertEqual(account_found.email,another_credential.email)
+		self.assertEqual(account_found.last_name,another_credential.last_name)
 
 #account exists function
 	def test_account_exists(self):
 
 		self.new_credential.save_credential()
-		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+		another_credential=Credential("James","Gitau","QWERTY")
 		
 		another_credential.save_credential()
 
-		exist_account=Credential.account_exist("jamesgitau@gmail.com")
+		exist_account=Credential.account_exist("Gitau")
 
 		self.assertTrue(exist_account)
 
 
+# 		self.new_credential=Credential("apiut","toel","QwertY","toelapiut7@gmail.com")
+
+# #creation of an initializing test 
+# 	def test_init(self):
+# 		self.assertEqual(self.new_credential.first_name,"apiut")
+# 		self.assertEqual(self.new_credential.last_name,"toel")
+# 		self.assertEqual(self.new_credential.password,"QwertY")
+# 		self.assertEqual(self.new_credential.email,"toelapiut7@gmail.com")
+
+# #testing if my credentials are being saved
+# 	def test_save_credential(self):
+# 		self.new_credential.save_credential()
+
+# 		self.assertEqual(len(Credential.login_credentials),1)
+
+# #tearing Down function to clear class variable
+# 	def tearDown(self):
+# 		Credential.login_credentials=[]
+
+# #saving multiple credentials 
+# 	def test_save_multiple_credentials(self):
+
+# 		self.new_credential.save_credential()
+
+# 		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+# 		another_credential.save_credential()
+
+# 		self.assertEqual(len(Credential.login_credentials),2)
+
+# #delete credential if someone doesn't need the account 
+# 	def test_delete_credential(self):
+
+# 		self.new_credential.save_credential()
+
+# 		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+# 		another_credential.save_credential()
+
+# 		self.new_credential.delete_credential()
+
+# 		self.assertEqual(len(Credential.login_credentials),1)
+
+# #access the account by password
+# 	def find_account_by_password(self):
+# 		self.new_credential.save_credential()
+
+# 		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+# 		another_credential.save_credential()
+
+# 		account_found=Credential.find_account("QWERTY")
+
+# 		self.assertEqual(account_found.email,another_credential.email)
+
+# #account exists function
+# 	def test_account_exists(self):
+
+# 		self.new_credential.save_credential()
+# 		another_credential=Credential("James","Gitau","QWERTY","jamesgitau@gmail.com")
+		
+# 		another_credential.save_credential()
+
+# 		exist_account=Credential.account_exist("jamesgitau@gmail.com")
+
+# 		self.assertTrue(exist_account)
 
 
 if __name__=="__main__":
