b2c8c2abae2c1744b1ea557fc06e77efc2ed572d
toelapiut <toelapiut7@gmail.com>
2017-10-14 13:10:41 +0300

    added functions for creation of accounts

--- run.py
+++ run.py
@@ -17,3 +17,34 @@ def function():
 	print(" --      --   ----------   ----------   ----------    -----------  ")
 	
 function()
+    #END OF HELLO ANIMATION 
+ #=======================================================================================
+
+
+#function that take three arguments  from class Credential
+def creat_user_account(fname,lname,password):
+
+	new_user_account=Credential(fname,lname,password)
+
+	new_user_account.save_credential()
+#---------------------------------------------------------
+
+#funtion for saving accounts
+def creat_accounts(aname,emails,pword):
+	new_account=Account(aname,emails,pword)
+
+	new_account.save_account()
+
+#---------------------------------------------------------
+
+#function to find a user account 
+def find_account(passwrd):
+	return Credential.find_account(passwrd)
+
+#---------------------------------------------------------
+
+#function to check if the account exists 
+def account exists(last_name):
+	return Credential.account_exist(last_name)
+
+#---------------------------------------------------------
