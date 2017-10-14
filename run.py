58b88e643bfaf39e66513864a63c9c560a83c001
toelapiut <toelapiut7@gmail.com>
2017-10-14 12:50:39 +0300

    credential function called

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3.6
 import random
+
 def function():
 	print(" __      __   _________    __           __            __________    ")
 	print("|  |    |  | |         |  |  |         |  |          /           \ ")
