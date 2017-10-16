0ec038ce9551ffb789444c47257bedf9cce44782
toelapiut <toelapiut7@gmail.com>
2017-10-16 10:09:22 +0300

    fix bug

--- run.py
+++ run.py
@@ -120,132 +120,147 @@ print('\n')
 
 #collection of user info
 global saved_user;
-
-def creating_account():
-	print("Hey! Hello! It's nice to have you around! You're about to change your life.")
-
-	print("\n")
-
-	print("New User")
-	print("-"*22)
+def main():
 	
 
-	print("Enter You first name!")
-	fname=input().lower().capitalize().strip()
-	print("-"*22)
-
-	print("Enter last name!")
-	lname=input().lower().capitalize().strip()
-	print("-"*22)
-
-	print("Key In your password")
-	psword=input().strip()
-	print("-"*22)
+	print("Hey! Hello! It's nice to have you around! You're about to change your life.")
+	
+	
 
 
-	saved_user=save_accountsx(creat_user_account(fname,lname,psword))
-	print("\n")
-	print(f"Thank you!! {fname}-{lname} You've been added to our list ")
+	while True:
+		print("Use the following codes : UA- create a new user Account, LN - Log In,EXIT -exit completely")
+		print("Enter a code")
+		shortcode=input().lower().strip()
+		print("\n")
+		print("\n")
+		if shortcode=="ua":
+			
+			# print("Use the following codes : UA- create a new user Account, LN - Log In,EXIT -exit completely")
+			print("#"*22)
+			print("\n")
 
-creating_account()
+			print("New User")
+			print("-"*22)
+			
 
+			print("Enter Your first name!")
+			fname=input().lower().capitalize().strip()
+			print("-"*22)
+
+			print("Enter last name!")
+			lname=input().lower().capitalize().strip()
+			print("-"*22)
+
+			print("Key In your password")
+			psword=input().strip()
+			print("-"*22)
+
+
+			saved_user=save_accountsx(creat_user_account(fname,lname,psword))
+			print("\n")
+			print(f"Thank you!! {fname}-{lname} You've been added to our list ")
+
+		
+		elif shortcode=="ln":
 #--------------------------------------------------------
 
+	#confirmation of user credentials
+		
+			print("Hello,LogIn!")
 
-def main():
-#confirmation of user credentials
-	
-	print("Hello,LogIn!")
+			
+			print("Enter Password")		
 
-	
-	print("Enter Password")
-
-	
-
-	user_password=input().strip()
-	print("-"*22)
+			user_password=input().strip()
+			print("-"*22)
 
 
-	if account_existx(user_password):
-		while True:
-			print("Well, Use the following codes : ca - create a new account, da - display contacts, fa -find a account, ex -exit the contact list, del-del account")
+			if account_existx(user_password):
+				while True:
+					print("Well, Use the following codes : ca - create a new account, da - display contacts, fa -find a account, ex -exit the contact list, del-del account")
 
-			print("Enter code")
-			usercode= input().strip().lower()
+					print("Enter code")
+					usercode= input().strip().lower()
 
-			if usercode=='ca':
+					if usercode=='ca':
 
-				print("Enter Account Name")
-				aname=input()
-				print("\n")
-				
-				print("Enter email")
-				print("-"*22)
-				emails=input()
-				print("\n")
+						print("Enter Account Name")
+						aname=input()
+						print("\n")
+						
+						print("Enter email")
+						print("-"*22)
+						emails=input()
+						print("\n")
 
-				print("Do you want to create your own password?Y/N")
+						print("Do you want to create your own password?Y/N")
 
-				choice=input().strip().lower()
+						choice=input().strip().lower()
 
-#generation of password
-				if choice=="n":
-					pword=random_password()
-					print(random_password())
+		#generation of password
+						if choice=="n":
+							pword=random_password()
+							print(random_password())
 
-				elif choice=="y":
-					print("Enter Password")
-					print("-"*22)
-					pword=input()
-				save_user_account(create_account1(aname,emails,pword))
-#----------------------------------------------------------------------
+						elif choice=="y":
+							print("Enter Password")
+							print("-"*22)
+							pword=input()
+						save_user_account(create_account1(aname,emails,pword))
+		#----------------------------------------------------------------------
 
-#displaying all the users information
-			elif usercode=="da":
-				if display_account():
+		#displaying all the users information
+					elif usercode=="da":
+						if display_account():
 
-					for accs in display_account():
-						print(f" Account Name:{accs.account_name} \n Email: {accs.emails} \n password: {accs.passwords} \n ======================")
-				else:
-					print("No search Account ..Try again.")
-#----------------------------------------------------------------------
+							for accs in display_account():
+								print(f" Account Name:{accs.account_name} \n Email: {accs.emails} \n password: {accs.passwords} \n ======================")
+						else:
+							print("No search Account ..Try again.")
+		#----------------------------------------------------------------------
 
-#deleting a specific contact
-			elif usercode=="del":
-				print("-"*10)
-				print("Enter -Account Name- to delete")
-				user_acc_name=input()
-				print("\n")
+		#deleting a specific contact
+					elif usercode=="del":
+						print("-"*10)
+						print("Enter -Account Name- to delete")
+						user_acc_name=input()
+						print("\n")
 
-				if  account_existed(user_acc_name): 
-					search_account=find_existing_account(user_acc_name)
-					delete_accounts(search_account)
-					print("\n")
+						if  account_existed(user_acc_name): 
+							search_account=find_existing_account(user_acc_name)
+							delete_accounts(search_account)
+							print("\n")
 
-				else:
-					print("Account doesnt exist")
-#----------------------------------------------------------------------
+						else:
+							print("Account doesnt exist")
+		#----------------------------------------------------------------------
 
 
-#find the contact and display it 	
-			elif usercode=="fa":
-			    print("Enter -Account Name-")
-			    user_acc_name=input()
-			    print("\n")
+		#find the contact and display it 	
+					elif usercode=="fa":
+					    print("Enter -Account Name-")
+					    user_acc_name=input()
+					    print("\n")
 
-			    if  account_existed(user_acc_name): 
-				    search_for_account=find_existing_account(user_acc_name)
-				    print(f"Account Name:{search_for_account.account_name} \n Email:{search_for_account.emails} \n Password:{search_for_account.passwords} \n =================================================")
-					#---------------------------------------------------------------------
-#Exiting from the program				
-			elif usercode=="ex":
-				print("Bye....Come Again!")
-				break			
+					    if  account_existed(user_acc_name): 
+						    search_for_account=find_existing_account(user_acc_name)
+						    print(f"Account Name:{search_for_account.account_name} \n Email:{search_for_account.emails} \n Password:{search_for_account.passwords} \n =================================================")
+							#---------------------------------------------------------------------
+		#Exiting from the program				
+					elif usercode=="ex":
+						print("Bye....Come Again!")
+						break	
 
+					else:
+						print("Please try again")
+						# elif shortcode=="exit":
+						# print("See ya'!!")	
+		else:
+			print("Please create an account with")
+			
 #-----------------------------------------------------------------------
-	else:
-		print("Please try again")
-
+	
 
 #====================================================================
 								#END
